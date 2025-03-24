c60a4cccd83309b024b637a5a6efe3a8c2f27175
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-03-24 14:11:18 +0000

    Initial refactor and add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cast',
+      version='0.1',
+      description='CIM App Studies Tools',
+      author='Iain Emsley',
+      author_email='iain.emsley@warwick.ac.uk',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=[ ],
+     )
