6f9365f785fa244df8c16241b413a55ee0a44d26
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-04-22 17:32:17 +0100

    Beginning refactor of manifest

--- manifest/manifests.py
+++ manifest/manifests.py
@@ -30,15 +30,26 @@ class Manifest():
 
             package = root.get('package')
             version = root.get('{http://schemas.android.com/apk/res/android}versionName')
-            
-            ps = ""
-            if permissions is not None and len(permissions) > 0:
-                ps = ';'.join(permissions)
-            fs = ""
-            if features is not None and len(features) > 0:
-                fs = ';'.join(features)
 
-            fh = open(extract_dir + "/manifest_" + directory + ".csv", 'w')
-            data = "{0}, {1}, {2}, {3}, {4}".format(directory, ps, fs, package, version)
-            fh.write(data)
-            fh.close()
+            ps = self.list_to_string(permissions)
+            fs = self.list_to_string(features)
+
+        self.write_manifest(directory, ps, fs, package, version)
+
+    def list_to_string(self, list_elements):
+        '''
+        Format the list into string for CSV
+        '''
+        elements = ""
+
+        if list_elements is not None and len(list_elements) > 0:
+            elements = ';'.join(features)
+
+        return elements
+
+    def write_manifest(self, directory, ps, fs, package, version):
+
+        fh = open(extract_dir + "/manifest_" + directory + ".csv", 'w')
+        data = "{0}, {1}, {2}, {3}, {4}".format(directory, ps, fs, package, version)
+        fh.write(data)
+        fh.close()
