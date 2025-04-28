e23a7ead6fd4c2b672a100af8c93afd7c9616605
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-04-28 16:58:17 +0100

    Added devices

--- localisation/localisation.py
+++ localisation/localisation.py
@@ -28,7 +28,6 @@ class Localisation():
         bpath = basepath + "/res/"
         for root, dirs, files in os.walk(bpath):
             path = root.split(os.sep)
-            #iso = iso_lang.search(root)
 
             if iso_lang.search(root): 
                 pkgs.add("/".join(path).replace(basepath.replace('/', '.'), ""))
@@ -38,17 +37,18 @@ class Localisation():
         """
             Merge the localised file with the main files
         """
-        return pd.merge(main, locatl, on="sha")
+        return pd.merge(main, local, on="sha")
 
-    def extract_locales(self, localised_file):
+    def extract_locales(self, local_df):
         """
             Extract local data from file
         """
 
-        local_df = localised_df
+        #local_df = localised_df
 
-        local_df["language"] = local_df["local"].map(extract_language)
-        local_df["country"] = local_df["local"].map(extract_country)
+        local_df["language"] = local_df["local"].map(self.extract_language)
+        local_df["country"] = local_df["local"].map(self.extract_country)
+        local_df["device"] = local_df["local"].map(self.extract_device)
 
         return local_df
 
@@ -77,3 +77,19 @@ class Localisation():
                 if country.startswith("r"): 
                     country = country.replace("r","")
         return country
+
+    def extract_device(self, values):
+        '''
+        
+        '''
+        device = []
+        device_vals = ["hdpi", 'xxhdpi', "v4", 'land', 'night']
+
+        for d in device_vals:
+            if d in values:
+                device.append(d)
+        
+        if len(device) > 1:
+            return "".join(device)
+
+        return ""
