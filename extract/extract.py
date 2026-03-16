d60fb8d56cba9b1996afa26c09323436e03a02ff
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2026-03-16 10:41:11 +0000

    Added Androguard extract

--- extract/extract.py
+++ extract/extract.py
@@ -23,3 +23,16 @@ class Decompile():
            Extract a single file
         '''
         pass
+
+    def extract_apk(self, path_to_apk):
+        """
+            Extract the APK using Androguard. 
+
+            It will return the APK and dex classes.
+        """
+        if not os.path.exists(path_to_apk):
+            raise Exception (f"APK does not exist {path_to_apk}. Please check the path.")
+
+        apk, d, dex = AnalyzeAPK(path_to_apk)
+
+        return (apk, d, dex)
