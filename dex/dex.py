7776cd4edeeef9077f92b51b30d5883db6edc17a
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2026-05-11 18:47:37 +0100

    Fix to cde

--- dex/dex.py
+++ dex/dex.py
@@ -2,8 +2,13 @@
 Functions to work on the DEX code
 """
 from androguard.core.dex import DEX
+import re
+#from androguard.core.analysis import Analysis
 
 class analyseDEX():
+
+    def __init__(self):
+        pass
 
     def extract(self, apk):
         '''
@@ -11,26 +16,35 @@ class analyseDEX():
         '''
         return DEX(apk)
 
-    def find_http_string(dex):
+    def strings(self, dexcode):
+        '''
+        Get strings from DEX code
+
+        Args:
+            dex - the DEX file. 
+        
+        Return:
+            list
+        '''
         strs = []
         try:
-            #strs = [string for string in dex.get_strings() if  re.findall(r'https?://\S+', string)]
-            for string in dex.get_strings():
+            for string in dexcode.get_strings():
                 strs.extend(re.findall(r'https?://\S+', string))
         except Exception:
-            print(Exception)
-            pass
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
 
         return strs
 
-    def callgraph(dx, class_name):
+    def callgraph(self, dx, class_name):
         '''
         Find the network callgraph
         '''
         try:
             CFG = nx.DiGraph()
 
-            for m in dx.find_methods(classname=class_name):
+            for m in Analysis(dx).find_methods(classname=class_name):
                 orig_method = m.get_method()
 
                 is_this_external = False
@@ -52,4 +66,17 @@ class analyseDEX():
 
             return CFG
         except Exception as e:
-            print(e)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+
+    def methods(self, dex):
+        return dex.get_methods()
+
+    def cg(self, dex, classname):
+        """
+        Larger analyis
+        See: class Analysis https://github.com/androguard/androguard/blob/master/androguard/core/analysis/analysis.py
+        """
+        a = Analysis(dex)
+        a.find_methods(classname)
