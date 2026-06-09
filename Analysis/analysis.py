d727092567f5306d302e1ea7579ab651f584abd5
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2026-06-09 20:27:09 +0100

    create analysis object

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -4,3 +4,19 @@ See: class Analysis https://github.com/androguard/androguard/blob/master/androgu
 """
 
 from androguard.core.analysis import Analysis
+
+class Analyse:
+
+    def create_analysis_object(self, dex):
+        """
+            Convert DEX object into analysis object
+        """
+        dx = Analysis()
+        dex_objects = []
+
+        dx.add(dex)
+        dex_objects.append(dex)
+
+        dx.create_xref()
+
+        return dx
