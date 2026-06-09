14e24fb9791b8eb458ef201305d0f0ff814e19a1
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2026-06-09 20:24:03 +0100

    Tidy DEX

--- dex/dex.py
+++ dex/dex.py
@@ -3,7 +3,9 @@ Functions to work on the DEX code
 """
 from androguard.core.dex import DEX
 import re
-#from androguard.core.analysis import Analysis
+import os
+
+from androguard.core.analysis import Analysis
 
 class analyseDEX():
 
@@ -15,6 +17,28 @@ class analyseDEX():
         Function to extract DEX code
         '''
         return DEX(apk)
+
+    def find_methods(self, dexcode, classname):
+        '''
+        Get strings from DEX code
+
+        Args:
+            dex - the DEX file. 
+        
+        Return:
+            list
+        '''
+        strs = []
+        try:
+            for string in dexcode.get_methods():
+                strs.extend(re.findall(r'https?://\S+', string))
+                
+        except Exception:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+
+        return strs
 
     def http_strings(self, dexcode):
         '''
@@ -71,12 +95,40 @@ class analyseDEX():
             print(exc_type, fname, exc_tb.tb_lineno)
 
     def methods(self, dex):
-        return dex.get_methods()
+        return [method.get_name() for method in dex.get_methods()]
+
+    def get_classes(self, dex, string_find):
+        '''
+        Function to find string in class name
+        '''
+        classes = []
+        for cls in dex.get_classes():
+            if string_find in cls.name:
+                classes.extend([cls.name])
+        return classes
 
     def cg(self, dex, classname):
         """
         Larger analyis
         See: class Analysis https://github.com/androguard/androguard/blob/master/androguard/core/analysis/analysis.py
         """
-        a = Analysis(dex)
-        a.find_methods(classname)
+        for d in dex.find_methods(classname):
+            pass
+
+    def dynamic_detection(self, dex):
+        '''
+           Finding dynamic loading classes. 
+           Loading apps?
+        '''
+        dynamic_methods = [
+            "dalvik.system.DexClassLoader",
+            "dalvik.system.PathClassLoader"
+        ]
+
+        dynamic = []
+
+        for method in dex.get_methods():
+            for dyn_method in dynamic_methods:
+                if dyn_method in str(method.get_code()):
+                    dynamic.extend(method.name)
+        return dynamic
