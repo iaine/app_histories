febb1b3346d050a58a5261405984035fbed79da2
iaine <iain.emsley@kellogg.ox.ac.uk>
2026-06-12 09:32:59 +0100

    Tidied another file import

--- src/cim_app_histories/ab/ab.py
+++ src/cim_app_histories/ab/ab.py
@@ -1,18 +1,32 @@
 """
 Methods for tracking AB testing
+
+AB_CLASSES is further described on the App Histories site
 """
 import os
 import re
 
 class AB():
 
+    AB_CLASSES = ['com.playnomics.android.sdk.Playnomics', 'com.abtasty', 'io.adapty',
+                  'com.adobe.marketing.mobile', 'com.amplitude',
+                  'com.apphud.ApphudSDK-Android', 'com.applause', 
+                  'com.apptimize.Apptimize', 'com.apptimize.ApptimizeTest',
+                  'com.batch.android', 'com.leanplum', 'com.configcat',
+                  'com.gameanalytics.sdk', 'com.gameofwhales.gow',
+                  'com.gameofwhales.sdk', 'com.google.firebase',
+                  'com.huawei.hwid', 'com.huawei.hms', 'com.huawei.agconnect',
+                  'com.huawei.updatesdk', 'com.kameleoon', 'com.launchdarkly',
+                  'com.mparticle', 'com.optimizely.ab', 'com.posthog',
+                  'io.qonversion.android.sdk', 'com.sensorsdata.abtest.SensorsABTest',
+                  'com.sensorsdata.abtest.SensorsABTestConfigOptions',
+                  'com.sensorsdata.analytics.android', 'io.split.client',
+                  'com.statsig.androidsdk', 'com.swrve.sdk.android', 'com.taplytics.sdk',
+                  'com.umeng.commonsdk', 'com.umeng.analytics.game', 'com.uxcam.UXCam',
+                  'com.uxcam.datamodel.UXConfig', 'com.vwo.mobile']
+    
     def __init__(self):
-        self.data = []
-        with open("ab_testing.csv", "r") as fh:
-            data = fh.readlines()
-            for l in data:
-                ln = l.split('\n')
-                self.data.append(ln[0].split(',')[0].replace('\ufeff','').replace('.*',''))
+        pass
 
     def find_ab_by_package(self, classes):
         '''
@@ -25,7 +39,7 @@ class AB():
         '''
 
         common = []
-        for tr in self.data:
+        for tr in self.AB_CLASSES:
             if any(tr in x for x in classes):
                 common.append(tr)
 
