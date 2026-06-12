9a77c733e39a9d0ba2629506d584067f5ff4a7ae
iaine <iain.emsley@kellogg.ox.ac.uk>
2026-06-12 10:28:09 +0100

    Added for post processing

--- src/cim_app_histories/general/helpers.py
+++ src/cim_app_histories/general/helpers.py
@@ -1,63 +1,20 @@
 """
 This is the "helpers" module.
 
-It contains a helper function.
+It contains a helper function for post-processing
 """
-import sys
 
 class Helper():
 
     def __init__(self):
         pass
 
-    def get_software_version(self, dataframe, version, software_version):
-        '''
-        Helper function to get software version
-        '''
-
-        dataframe[software_version] = dataframe[version].apply(self._software_version)
-
-        return dataframe
-
-    def _software_version(self, softver):
-        '''
-        Function to identify and clean software. 
-        Works against the constraint of needing numeric values for graphs. 
-        '''
-        spl_ver = softver.split(".")
-
-        if len(spl_ver) > 1:
-            if "_" in spl_ver[-1]:
-                _end = spl_ver[-1].split("_")[0]
-                spl_ver[1:] += _end
-            
-            return float(spl_ver[0] +  "." + "".join(spl_ver[1:]) )
-        else:
-            return float(softver)
-
-
-    def write_pkg_csv(self, pkg_name, data, filename):
-        """
-        
-        :param pkg_name - Package Name
-        :param data - data to be written
-        :param filename - CSV filename
-        """
-        if type(data) != list:
-            print("Incorrect type. ")
-            sys.exit(2)
-
-        with open(os.path.join(filename + ".csv"), "w+") as fh:
-            if len(p[0]) == 1:
-                for p in data:
-                    if p != "": fh.write("{}, {}\n".format(pkg_name, p))
-            elif len(p[0]) == 2:
-                for p in data:
-                    if p != "": fh.write("{}, {}, {}\n".format(pkg_name, p[0],p[1]))
 
     def list_similarity(self, dict_of_lists):
         '''
-        Functions to compare lists to determine changes
+        Functions to compare lists to determine changes.
+        Run this for post-processing as it relies on a list
+        of permissions, activities, or intents to be present
         :param dict_of_lists - dictionary of lists
         '''
         current = []
@@ -68,8 +25,8 @@ class Helper():
             else:
                 s1 = set(current)
                 s2 = set(test)
-                added = list(s1.intersection(s2))
-                removed = list(s2.intersection(s1))
+                added = list(s2.intersection(s1))
+                removed = list(s1.intersection(s2))
                 changes[test] = {"added": ";".join(added), "remove":";".join(removed)}
             current = dict_of_lists[test]
         return changes
