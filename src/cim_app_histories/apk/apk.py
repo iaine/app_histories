cfcde430652ed29fad1aa3cee01ddafb17bc1209
iaine <iain.emsley@kellogg.ox.ac.uk>
2026-06-12 10:28:46 +0100

    Moved software version

--- src/cim_app_histories/apk/apk.py
+++ src/cim_app_histories/apk/apk.py
@@ -1,4 +1,11 @@
+"""
+Methods on the APK class. This is the highest level of extractin. 
+Usually to get metadata and format it.
+"""
+import sys
+
 from androguard.core.apk import APK
+
 
 class extractAPK():
 
@@ -43,3 +50,27 @@ class extractAPK():
         Get name code for this version
         '''
         return apk.get_androidversion_name()
+    
+    #---------------------------------------
+    # Methods
+    #---------------------------------------
+    def get_software_version(self, softver):
+        '''
+            Helper function to get software version
+            Software might be semantically versioned - 7.12.34 -
+            or 7.1234. The former stops this form being plotted on
+            a graph as it is read incorrectly. So here we provide a
+            flattened version for plotting as well as the "real" version.
+
+            :param softver - the retrieved version, 
+        '''
+        spl_ver = softver.split(".")
+
+        if len(spl_ver) > 1:
+            if "_" in spl_ver[-1]:
+                _end = spl_ver[-1].split("_")[0]
+                spl_ver[1:] += _end
+            
+            return float(spl_ver[0] +  "." + "".join(spl_ver[1:]) )
+
+        return float(softver)
