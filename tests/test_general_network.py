008dbe14e0be5467bc9a98ce721d655705ab8a87
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2026-07-22 10:10:33 +0100

    Fised test import

--- tests/test_general_network.py
+++ tests/test_general_network.py
@@ -2,10 +2,10 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
 
-import cim_app_histories.general.network as nt
+from cim_app_histories.general.network import NetworkAddr
 
 def test_address_from_host():
     hostname = "warwick.ac.uk"
-    net = nt.Network()
-    n = net.addr_from_name(hostname)
+    nt = NetworkAddr()
+    n = nt.addr_from_name(hostname)
     assert "137.205.28.41" == n
